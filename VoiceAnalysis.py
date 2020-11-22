c778fb915f5802a096be66de1c249a7815deea4a
YK-Orfeluna <25401452+YK-Orfeluna@users.noreply.github.com>
2020-11-22 10:00:36 +0900

    corresoponding to {raw, aiff, aifc} files

--- VoiceAnalysis.py
+++ VoiceAnalysis.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import argparse
+import aifc
 from os import makedirs
 from os.path import basename, splitext
 import numpy as np
@@ -28,8 +29,8 @@ def get_args():
     parser.add_argument("inlist")
     parser.add_argument("outd")
     ### VAD options
-    parser.add_argument("--vad_mode", type=int, choices=(0, 1, 2, 3), default=1, \
-        help="To see py-webrtcvad's document; default is '1'.")
+    parser.add_argument("--vad_mode", type=int, choices=(-1, 0, 1, 2, 3), default=1, \
+        help="To see py-webrtcvad's document. If you choose '-1', VAD will be not used; default is '1'.")
     parser.add_argument("--vad_frame_duration", type=int, choices=(10, 20, 30), default=10, \
         help="To see py-webrtcvad's document; default is '10'[ms].")
     ### F0 options
@@ -45,9 +46,16 @@ def get_args():
     parser.add_argument("--demo", action="store_true", default=False, \
         help="If activate, demo will be displayed.")
     parser.add_argument("--demo_index", type=int, default=-1)
+    # options for raw-file
+    parser.add_argument("--sampling_rate", type=int, choices=(8000, 16000, 32000, 48000), \
+        help="To choose raw inputs' sampling rate.")
+    parser.add_argument("--bit_rate", type=int, choices=(16, ), \
+        help="To choose raw inputs' bit rate.")
     return parser.parse_args()
 
 def VAD(data, rate):
+    if args.vad_mode==-1:
+        return data.copy(), np.ones_like(data)
     vad = webrtcvad.Vad()
     vad.set_mode(args.vad_mode)
     i = 0
@@ -104,8 +112,7 @@ def compute_stats(data):
 def demo(inf=None):
     if inf is None:
         inf = pysptk.util.example_audio_file()
-    rate, data = wavfile.read(inf)
-    assert rate in (8000, 16000, 32000, 48000), "py-webrtcvad corresponds to {8000, 16000, 32000, 48000} Hz. Your input is %s Hz." %rate
+    rate, data = read_file(inf)
     vad_wav, vad_flag = VAD(data, rate)
     wavfile.write("%s/original.wav" %args.outd, rate, data)
     wavfile.write("%s/vad.wav" %args.outd, rate, vad_wav)
@@ -115,7 +122,7 @@ def demo(inf=None):
     # left side
     plt.subplot(321)
     plt.plot(data, color="blue")
-    plt.title("original PCM (sampling rate: %s Hz)" %rate)
+    plt.title("original signal (sampling rate: %s Hz)" %rate)
     plt.ylim(max(np.abs(data))*-1, max(np.abs(data)))
     plt.subplot(323)
     plt.plot(vad_flag, color="red")
@@ -124,7 +131,7 @@ def demo(inf=None):
     # right side
     plt.subplot(322)
     plt.plot(vad_wav, color="cyan")
-    plt.title("PCM after VAD")
+    plt.title("signal after VAD")
     plt.ylim(max(np.abs(data))*-1, max(np.abs(data)))
     plt.subplot(324)
     plt.plot(f0, color="green")
@@ -141,21 +148,44 @@ def demo(inf=None):
     print("%s is saved." %outf)
     return 0
 
+def read_file(inf):
+    ext = splitext(inf)[1].lower()
+    if ext==".wav":
+        rate, data = wavfile.read(inf)
+    elif ext in (".aifc", ".aiff"):
+        with aifc.open(inf, "rb") as fd:
+            if fd.getsamplewidth()==2:
+                dtype = np.int16
+            elif fd.getsamplewidth()==4:
+                dtype = np.int32
+            else:
+                assert False, "This script corresponds to 16 or 32 bit signed-integer only."
+            data = np.frombuffer(fd.readframes(fd.getnframes()), dtype=dtype)
+            rate = fd.getframerate()
+    else:
+        if args.bit_rate is None or args.sampling_rate is None:
+            assert False, "You have to input $sampling_rate and $bit_rate."
+        dtype = "int" + str(args.bit_rate)
+        with open(inf, "rb") as fd:
+            data = np.frombuffer(fd.read(), dtype=dtype)
+        rate = args.sampling_rate
+    assert rate in (8000, 16000, 32000, 48000), "py-webrtcvad corresponds to {8000, 16000, 32000, 48000} Hz. Your input is %s Hz." %rate
+    return rate, data
+
 header="filename\tmean\tvariance\tmedian\tmode\tmode_ratio\tmin\tmax\tq75\tq25\tIQR\tskew\tkurtosis\n"
 def analysis(infs):
     vad_length = "speech_length[sec.]\n"
     f0_out = header
     power_out = header
     for inf in tqdm(infs):
-        rate, data = wavfile.read(inf)
-        assert rate in (8000, 16000, 32000, 48000), "py-webrtcvad corresponds to {8000, 16000, 32000, 48000} Hz. Your input is %s Hz." %rate
+        rate, data = read_file(inf)
         vad_wav, _ = VAD(data, rate)
         vad_length += "%s\n" %(vad_wav.shape[0] / rate)
-        f0 = F0(data, rate)
+        f0 = F0(vad_wav, rate)
         f0 = f0[f0>-1]
         f0_stats = compute_stats(f0)
         f0_out += inf+"\t"+"\t".join(map(str, f0_stats))+"\n"
-        power = Power(data, rate)
+        power = Power(vad_wav, rate)
         power_stats = compute_stats(power)
         power_out  += inf+"\t"+"\t".join(map(str, power_stats))+"\n"
     for name, out in zip(("f0", "power", "speech_length"), (f0_out, power_out, vad_length)):
@@ -166,15 +196,17 @@ def analysis(infs):
     return 0
 
 def main():
-    if args.demo:
-        demo()
-        return 0
     with open(args.inlist) as fd:
         infs = sorted(fd.read().strip().split("\n"))
-    if args.demo_index>=0:
-        demo(infs[args.demo_index])
+    if args.demo:
+        if args.demo_index==-1:
+            inf = None
+        else:
+            inf = infs[args.demo_index]
+        demo(inf)
         return 0
-    analysis(infs)
+    else:
+        analysis(infs)
 
 if __name__=="__main__":
     args = get_args()
